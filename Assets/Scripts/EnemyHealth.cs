944753341ce9e1b36db15af9794610251369b7d5
GiovaneDvooranen <giovanedvooranen@gmail.com>
2026-09-24 11:24:28 -0300

    commit

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private Vector3 originalScale;
     public int hp = 50;
 
     public int scoreValue = 10;
 
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     public void TakeDamage(int damage)
     {
         hp -= damage;
+
+        PlayHitEffect();
 
         if (hp <= 0)
         {
@@ -17,4 +26,17 @@ public class EnemyHealth : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void PlayHitEffect()
+    {
+        transform.DOKill();
+
+        transform.DOPunchScale(
+        Vector3.one * 0.4f,
+        0.2f,
+        5,
+        0.5f
+        );
+    }
+
 }
